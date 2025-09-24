6076832f7bc64b41410c81fa043385659ca73745
Parmanvir Singh <156245617+parmanvirsingh@users.noreply.github.com>
2025-09-24 14:38:26 -0700

    .

--- labs/LAB_01/Hand_C.cs
+++ labs/LAB_01/Hand_C.cs
@@ -7,21 +7,29 @@ using GDIDrawer;
 
 namespace LAB_FINAL_01
 {
-    public enum HandType {Rock,Paper,Scissors}
+    public enum HandType {Rock,Paper,Scissors}//maing the enum of game 
     public class HandS
     {
-        public struct Hand
+        public struct Hand//ssetting the constanst so you can change  values after just from here
         {
             public const float HAND_SIZE = 45f;
             public const float MIN_SPEED = 0.5f;
             public const float MAX_SPEED = 1.5f;
-            public const int SIZE = 45; 
+            public const int SIZE = 45; //size of the circle
 
-            public HandType Type { get; set; }
+            public HandType Type { get; set; }//automic properties for getting and setting value during game
             public PointF Position { get; set; }
             public PointF Velocity { get; set; }
             public Color Color { get; set; }
 
+            /// <summary>
+            /// Creating the random position elements like sissor
+            /// paper rock in the form of ewnum hand 
+            /// </summary>
+            /// <param name="random"></param>
+            /// <param name="areaWidth"></param>
+            /// <param name="areaHeight"></param>
+            /// <returns></returns>
             public static Hand CreateRandom(Random random, int areaWidth, int areaHeight)
             {
                 HandType type = (HandType)random.Next(0, 3);
@@ -35,6 +43,13 @@ namespace LAB_FINAL_01
                 };
             }
 
+            /// <summary>
+            /// TO CHECKING THE OVERLAP LIKE ON PAPER TO PAPER SISOR TO PAPER 
+            /// PAPER RORCK TO ROCK
+            /// </summary>
+            /// <param name="hand1"></param>
+            /// <param name="hand2"></param>
+            /// <returns></returns>
             public static bool CheckOverlap(Hand hand1, Hand hand2)
             {
                 if (hand1.Type == hand2.Type) return false;
@@ -45,6 +60,12 @@ namespace LAB_FINAL_01
 
                 return distance < HAND_SIZE;
             }
+            /// <summary>
+            /// DetermineWinner CHECKING THE WINNER AFTER THE COLISSION ON ROCK WINS OVER SISSORS
+            /// </summary>
+            /// <param name="type1"></param>
+            /// <param name="type2"></param>
+            /// <returns></returns>
             public static HandType DetermineWinner(HandType type1, HandType type2)
             {
                 return (type1, type2) switch
@@ -54,7 +75,12 @@ namespace LAB_FINAL_01
                     (HandType.Scissors, HandType.Paper) => HandType.Scissors,(HandType.Paper, HandType.Scissors) => HandType.Scissors,_ => type1
                 };
             }
-
+            /// <summary>
+            /// NOTHING JUST AFTER COLLIOION WHO WINS ALSO CHANGE THE COLOR
+            /// LIKE ROCK FOR RED PAPER GREEN 
+            /// </summary>
+            /// <param name="type"></param>
+            /// <returns></returns>
             public static Color GetColorForType(HandType type)
             {
                 return type switch
