e4e4c2a376b88fd6582aa3bba5c959371f635cfc
Parmanvir Singh <156245617+parmanvirsingh@users.noreply.github.com>
2025-09-24 15:41:39 -0700

    0.6

--- labs/LAB_01/Form1.cs
+++ labs/LAB_01/Form1.cs
@@ -7,21 +7,20 @@ namespace LAB_FINAL_01
     {
 
           SimulationManager _simulation;
-          System.Windows.Forms.Timer _timer;
-          int _animation_Interval = SimulationManager.DEFAULT_INTERVAL;
+          System.Windows.Forms.Timer _timer;//creating the timer
+          int _animation_Interval = SimulationManager.DEFAULT_INTERVAL;//getting the interval from default val 
 
 
-          Dictionary<HandType, Color> _colorMap = new() { { HandType.Rock, Color.Red }, { HandType.Paper, Color.Green }, { HandType.Scissors, Color.Blue } };
+          Dictionary<HandType, Color> _colorMap = new() { { HandType.Rock, Color.Red }, { HandType.Paper, Color.Green }, { HandType.Scissors, Color.Blue } };//making the manual dictionary 
 
 
         public void Initialt()
         {
            
-
+            //setting the form things like label text
             lblInterval.Text = $"Interval: {_animation_Interval}ms (Use Mouse Wheel)";
-            Controls.AddRange(new Control[] { lblHandCount, numHandCount, btnStart, lblStatus, lblInterval });
             btnStart.Click += (s, e) => StartSimulation((int)numHandCount.Value);
-            numHandCount.ValueChanged += (s, e) => lblStatus.Text = "Ready to start";
+            numHandCount.ValueChanged += (s, e) => lblStatus.Text = "Ready to start";//aslo adding to even args like e
             Text = "Rock-Paper-Scissors Simulation";
         }
 
@@ -29,11 +28,10 @@ namespace LAB_FINAL_01
         {
             InitializeComponent();
             Initialt();
-            _simulation = new SimulationManager();
-            _timer = new System.Windows.Forms.Timer();
-            _timer.Tick += Timer_Tick;
-            UpdateInterval();
-
+            _simulation = new SimulationManager();//setting the class
+            _timer = new System.Windows.Forms.Timer();//timer
+            _timer.Tick += Timer_Tick;//adding the delegate
+            UpdateInterval();//for the interval
             // Set up mouse wheel event for interval control
             MouseWheel += MainForm_MouseWheel;
         }
@@ -43,9 +41,9 @@ namespace LAB_FINAL_01
         {
             try
             {
-                _simulation.Initialize(handCount);
+                _simulation.Initialize(handCount);//giving the count 
                 _timer.Start();
-                UpdateStatus($"Running: {handCount} hands");
+                UpdateStatus($"Running: {handCount} hands");//just the label
             }
             catch (Exception ex)
             {
@@ -65,6 +63,7 @@ namespace LAB_FINAL_01
             foreach (HandS.Hand hand in _simulation.Hands)
             {
                 Color color = _colorMap[hand.Type];
+                //doing the same thing like in the other class render method do draw and text on ball
                 _simulation._drawer.AddCenteredEllipse((int)hand.Position.X,(int)hand.Position.Y,Hand.SIZE,Hand.SIZE,color);
                 _simulation._drawer.AddText(SimulationManager.GetTextForType(hand.Type), 15, (int)hand.Position.X - 5, (int)hand.Position.Y - 5, (int)Hand.HAND_SIZE / 2, (int)Hand.HAND_SIZE / 2, Color.White);
 
@@ -79,29 +78,45 @@ namespace LAB_FINAL_01
                 
             }
         }
-
+        /// <summary>
+        /// Mouse wheel this is doing the interval speed default is 50
+        /// from each chanhge it gonna change by +5_-5
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MainForm_MouseWheel(object? sender, MouseEventArgs e)
         {
             // Mouse wheel control for animation interval
             _animation_Interval += e.Delta > 0 ? -5 : 5;
-            _animation_Interval = Math.Clamp(_animation_Interval, SimulationManager.MIN_INTERVAL, SimulationManager.MAX_INTERVAL);
+            _animation_Interval = Math.Clamp(_animation_Interval, SimulationManager.MIN_INTERVAL, SimulationManager.MAX_INTERVAL);//making it in the range 
             UpdateInterval();
         }
 
-
+        /// <summary>
+        /// just changing the text of the borm label making it in fuction
+        /// just just it handy
+        /// </summary>
         private void UpdateInterval()
         {
-            _timer.Interval = _animation_Interval;
-            if (Controls.Count > 0 )
-                lblInterval.Text = $"Interval: {_animation_Interval}ms (Use Mouse Wheel)";
+            _timer.Interval = _animation_Interval;       
+            lblInterval.Text = $"Interval: {_animation_Interval}ms (Use Mouse Wheel)";
         }
-
+        /// <summary>
+        /// same for this one 
+        /// </summary>
+        /// <param name="status"></param>
         private void UpdateStatus(string status)
         {
-            if (Controls.Count > 2 && Controls[3] is Label lbl)
-                lbl.Text = status;
+            
+                lblStatus.Text = status;
         }
 
+        /// <summary>
+        /// Exiting the program if exit button is click addition just some times
+        /// cause problem if not fully close
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             _timer.Stop();
